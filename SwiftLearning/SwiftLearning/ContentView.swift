94df6bda3c8d8b29d194918f0c02e63af607fc1a
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-08-07 21:01:57 +0530

    chore :gradient colors

--- SwiftLearning/SwiftLearning/ContentView.swift
+++ SwiftLearning/SwiftLearning/ContentView.swift
@@ -22,21 +22,34 @@ struct ContentView: View {
 //            }
 //        }
         
-        ZStack {
+//         ZStack {
             
-            VStack(spacing: 0) {
-//                Color.red
-//                    .frame(width: 200, height: 200)
-//                Color.red
-                Color.blue.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-            }
+//             VStack(spacing: 0) {
+// //                Color.red
+// //                    .frame(width: 200, height: 200)
+// //                Color.red
+//                 Color.blue.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+//             }
 
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+//             Text("Your content")
+//                 .foregroundStyle(.secondary)
+//                 .padding(50)
+//                 .background(.ultraThinMaterial)
+//         }
+//         .ignoresSafeArea()
+
+
+LinearGradient(stops: [
+    Gradient.Stop(color: .white, location: 0.45),
+    Gradient.Stop(color: .black, location: 0.55),
+], startPoint: .top, endPoint: .bottom)
+        
+        
+        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+        
+        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+
+
     }
 }
 
