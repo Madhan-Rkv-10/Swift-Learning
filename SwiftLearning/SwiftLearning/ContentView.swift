f36a7e26745f1b8c37470f59312048cc57c66ad4
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-08-07 20:56:46 +0530

    chore : frame ,safearea, color , container and constraints

--- SwiftLearning/SwiftLearning/ContentView.swift
+++ SwiftLearning/SwiftLearning/ContentView.swift
@@ -12,15 +12,31 @@ struct ContentView: View {
     @State private var selectedStudent = "Harry"
 
     var body: some View {
-        NavigationStack {
-            Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }.navigationTitle("Text")
+//        NavigationStack {
+//            Form {
+//                Picker("Select your student", selection: $selectedStudent) {
+//                    ForEach(students, id: \.self) {
+//                        Text($0)
+//                    }
+//                }.navigationTitle("Text")
+//            }
+//        }
+        
+        ZStack {
+            
+            VStack(spacing: 0) {
+//                Color.red
+//                    .frame(width: 200, height: 200)
+//                Color.red
+                Color.blue.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
             }
+
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
+        .ignoresSafeArea()
     }
 }
 
