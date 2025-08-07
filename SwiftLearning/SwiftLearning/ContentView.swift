b7057197480646fb07d1be13853a41c876763267
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-08-07 21:03:00 +0530

    text gradient

--- SwiftLearning/SwiftLearning/ContentView.swift
+++ SwiftLearning/SwiftLearning/ContentView.swift
@@ -39,17 +39,21 @@ struct ContentView: View {
 //         .ignoresSafeArea()
 
 
-LinearGradient(stops: [
-    Gradient.Stop(color: .white, location: 0.45),
-    Gradient.Stop(color: .black, location: 0.55),
-], startPoint: .top, endPoint: .bottom)
-        
-        
-        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
-        
-        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+//LinearGradient(stops: [
+//    Gradient.Stop(color: .white, location: 0.45),
+//    Gradient.Stop(color: .black, location: 0.55),
+//], startPoint: .top, endPoint: .bottom)
+//
+//        
+//        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+//        
+//        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+//
 
-
+        Text("Your content")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundStyle(.white)
+            .background(.red.gradient)
     }
 }
 
